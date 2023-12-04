20e38cbef17753f4642de51f8f46e53821bc9ad5
OGabrielfo <ogabrielfo.silva@gmail.com>
2023-12-04 11:12:02 -0300

    Editar, listar e novo produto
    Alterações de design realizadas nas páginas de Novo produto, Editar produto e Listar produtos.
    Alguns componentes tem o parâmetro style em sua tag para melhor personalização.

--- editar-produtos.php
+++ editar-produtos.php
@@ -10,25 +10,31 @@
     // Retorno visual com as informações do item a ser editado
     $row = $res->fetch_object();
 ?>
-
-<form action="?page=actions" method="POST">
-    <input type="hidden" name="acao" value="editar">
-    <input type="hidden" name="id" value="<?php print $row->id; ?>">
-    <div class="mb-3">
-        <label for="">Nome do Produto</label>
-        <input type="text" name="nome" value="<?php print $row->nome; ?>" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Quantidade</label>
-        <input type="number" name="quant" value="<?php print $row->quantidade; ?>" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Preço</label>
-        <input type="number" format="currency" precision="2" step="0.01" name="preco" value="<?php print $row->preco; ?>" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Descrição</label>
-        <input type="text" name="descr" value="<?php print $row->descricao; ?>" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
+<div class="container bg-secondary rounded p-3">
+    <form action="?page=actions" method="POST">
+        <div class="row">
+            <!-- Input do tipo hidden com o que será chamado ao enviar o formulário para que não apareça na url -->
+            <input type="hidden" name="acao" value="editar">
+            <input type="hidden" name="id" value="<?php print $row->id; ?>">
+            <div class="mb-3 col">
+                <label for="">Nome do Produto</label>
+                <input type="text" name="nome" value="<?php print $row->nome; ?>" class="form-control">
+            </div>
+            <div class="mb-3 col-2">
+                <label for="">Quantidade</label>
+                <input type="number" name="quant" value="<?php print $row->quantidade; ?>" class="form-control">
+            </div>
+            <div class="mb-3 col-2">
+                <label for="">Preço</label>
+                <input type="number" format="currency" precision="2" step="0.01" name="preco" value="<?php print $row->preco; ?>" class="form-control">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="">Descrição</label>
+            <textarea name="descr" rows="3" style="resize: none" class="form-control"><?php print $row->descricao; ?></textarea>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary" style="padding: 5px 30px;">Salvar</button>
+        </div>
+    </form>
+</div>
--- novo-produto.php
+++ novo-produto.php
@@ -1,21 +1,28 @@
 <h1>Novo Produto</h1>
-<form action="?page=actions" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
-    <div class="mb-3">
-        <label for="">Nome do Produto</label>
-        <input type="text" name="nome" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Quantidade</label>
-        <input type="number" name="quant" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Preço</label>
-        <input type="number" format="currency" precision="2" step="0.01" name="preco" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label for="">Descrição</label>
-        <textarea name="descr" class="form-control" rows="3"  style="resize: none"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Salvar</button>
-</form>
+<div class="container bg-secondary rounded p-3">
+    <form action="?page=actions" method="POST">
+        <div class="row">
+            <!-- Input do tipo hidden com o que será chamado ao enviar o formulário para que não apareça na url -->
+            <input type="hidden" name="acao" value="cadastrar">
+            <div class="mb-3 col">
+                <label>Nome do Produto</label>
+                <input type="text" name="nome" class="form-control">
+            </div>
+            <div class="mb-3 col-2">
+                <label>Quantidade</label>
+                <input type="number" name="quant" class="form-control">
+            </div>
+            <div class="mb-3 col-2">
+                <label>Preço</label>
+                <input type="number" format="currency" precision="2" step="0.01" name="preco" class="form-control">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label>Descrição</label>
+            <textarea name="descr" class="form-control" rows="3"  style="resize: none"></textarea>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary" style="padding: 5px 30px;">Salvar</button>
+        </div>
+    </form>
+</div>
